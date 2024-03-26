948c0f2de55b6fdfeb72ab45f5b43ef8b6797f69
Elianarve <elirve2@outlook.es>
2024-03-26 14:39:14 +0100

    add-model

--- backend/controllers/newsController.ts
+++ backend/controllers/newsController.ts
@@ -9,3 +9,45 @@ export const getNews = async(req: Request, res: Response) => {
         res.status(500).json({error: 'Internal Server Error'});
     }
 }
+
+export const deleteNews = async (req: Request, res: Response) => {
+    const newsId = req.params.id;
+    try {
+            await NewsModel.destroy({ where: { id: newsId }});
+        return res.status(201).send({ message: 'News deleted successfully' });
+
+    } catch (error) {
+        return res.status(500).send({ error: 'Internal Server Error' });
+    }
+};
+
+
+export const createdNews = async (req: Request, res: Response) => {
+    try {
+        const createdNewNews = await NewsModel.create(req.body)       
+        res.status(201).json(createdNewNews);
+    }catch(error){
+        return res.status(500).send({ error: 'Internal Server Error' });
+    }
+}
+
+export const updateNews = async (req: Request, res: Response) => {   
+    const newsId = req.params.id;
+    try {
+        await NewsModel.update(req.body,{  where: {id: newsId}});
+        res.status(200).json({message: ` News: ${newsId}, Successfully updated`});
+    } catch(error) {
+        res.status(500).json({error: 'Internal Server Error'});
+    }   
+}
+
+
+export const getOneNews = async (req: Request, res: Response) =>{
+    const newsId = req.params.id;
+    try {
+        const news = await NewsModel.findOne({ where: { id: newsId}});
+        res.status(200).json(news);
+    } catch(error) {
+        res.status(500).json({error: 'Internal Server Error'});
+    }   
+}
--- backend/routes/newsRouter.ts
+++ backend/routes/newsRouter.ts
@@ -1,8 +1,16 @@
 import express from 'express';
-import { getNews } from '../controllers/newsController';
+import { deleteNews, getNews, createdNews, updateNews, getOneNews } from '../controllers/newsController';
 
 const router = express.Router();
 
 router.get('/', getNews);
+
+router.delete('/:id', deleteNews);
+
+router.post('/', createdNews);
+
+router.put('/:id', updateNews);
+
+router.get('/:id', getOneNews);
 
 export default router;
