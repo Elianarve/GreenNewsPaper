22d40e1bf7ca11cec7d063acdc8aa79ac6a498b8
SarayAnta <s.rodriguezanta@gmail.com>
2024-04-01 13:03:03 +0200

    add login

--- backend/controllers/autController.ts
+++ backend/controllers/autController.ts
@@ -14,6 +14,25 @@ export const registerUser = async (req: Request, res: Response) => {
     }
 }
 
+export const loginUser = async (req: Request, res: Response) => {
+    try {
+        const user: any = await UsersModel.findOne({ where: {email: req.body.email } });
+        if(!user) {
+            return res.status(404).send({ error: "Usuario no encontrado"});
+        }
+
+        const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(401).send( {error: "Contraseña incorrecta"});
+        }
+
+        res.status(200).send({ message: "Inicio de sesión correcto"})
+    } catch(error) {
+        return res.status(500).send({ error: "Internal Server Error"});
+    }
+}
+
 
 
 //validaciones
--- backend/routes/authRouter.ts
+++ backend/routes/authRouter.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { registerUser } from '../controllers/autController';
+import { registerUser, loginUser } from '../controllers/autController';
 
 const router = express.Router();
 
 router.post('/', registerUser);
+router.post('/login', loginUser)
 
 export default router;
