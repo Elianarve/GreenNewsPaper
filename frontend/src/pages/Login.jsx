9c7b79eed3d069492534a499c82d65a3ba328a6a
BeatrizMercado <bea.mercado.lop@gmail.com>
2024-04-03 12:46:04 +0200

    give-style-to-login-page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+// import {useLoaderData} from 'react-router-dom';
+import React from 'react';
+import LoginForm from '../components/LoginForm.jsx';
+import arrow from '../assets/arrow.svg'; 
+import { Link } from 'react-router-dom';
 
-function Login() {
-  return (
-    <div>Login</div>
-  )
+const Login = () => {
+  // const news = useLoaderData();
+
+ return (
+  <>
+    <div className='flex items-center justify-center min-h-screen bg-neutral-900'>
+      <div className='text-center mt-20'>
+        <Link to='/'>
+          <img className='ml-8 mt-3 absolute' src={arrow}></img>
+        </Link>
+          <h1 className='font-poppins font-medium text-4xl text-white mb-20'>Iniciar sesión</h1>
+        <LoginForm />
+      </div>  
+    </div>
+  </>  
+ );
 }
-
-export default Login
+export default Login;
