8cf967e5ca6d97279a48a981bfd391f7da41334d
BeatrizMercado <bea.mercado.lop@gmail.com>
2024-04-03 09:47:49 +0200

    change-colours

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Spline from '../components/Spline.jsx'
-import Nav from '../components/Nav.jsx'
+// import Nav from '../components/Nav.jsx'
 
 const Landing = () => {
  return (
   <>
-  <Nav/>
-  <main className="grid grid-cols-2 gap-10 place-content-center bg-neutral-900">
-    <section className="items-center mx-32 mt-10 justify-center min-h-screen bg-gray-00">
-      <h1 className="font-poppins font-semibold mb-0 text-6xl mb-4 leading-normal text-white">¡Explora el <br/>mañana <br/><span className="text-rose-600">hoy mismo!</span></h1>
-      <p className='font-poppins mb-5 text-lg text-white'>Alimenta tu curiosidad diaria: <br/>tecnología, IA, blockchain y más.</p>
-      <button className="w-full font-poppins bg-gradient-to-r rounded-2xl from-purple-600 to-rose-600 hover:from-rose-600 hover:to-purple-600 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5">
-          Regístrate
-      </button>
-      <Link to ="/login">
-        <button className="w-full font-poppins bg-white rounded-2xl hover:from-rose-600 hover:to-purple-600 text-black py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          Iniciar sesión
-        </button>
-      </Link>
-    </section>
-    <section className='mr-20 mb-20'>
-      <Spline/>
-    </section>
+  {/* <Nav/> */}
+  <main className="grid grid-cols-2 place-content-center bg-neutral-900">
+      <section className="items-center mx-32 mt-20 justify-center min-h-screen bg-gray-00">
+          <h1 className="font-poppins font-semibold mb-0 text-6xl mb-4 text-white">¡Explora el <br/>mañana <br/><span className="text-[#FB005A]">hoy mismo!</span></h1>
+          <p className='font-poppins mb-5 text-lg text-white'>Alimenta tu curiosidad diaria: <br/>tecnología, IA, blockchain y más.</p>
+          <button className="w-full font-poppins bg-gradient-to-r rounded-2xl from-[#B800B0] to-[#FB005A] hover:from-[#FB005A] hover:to-[#B800B0] text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5 w-11/12 h-12">
+            Regístrate
+          </button>
+          <Link to ="/login">
+          <button className="w-full font-poppins font-semibold bg-white rounded-2xl hover:from-[#FB005A] hover:to-purple-600 text-black py-2 px-4 rounded focus:outline-none focus:shadow-outline w-11/12 h-12">
+            Iniciar sesión
+          </button>
+          </Link>
+      </section>
+      <section className='mr-20 mb-20'>
+          <Spline/>
+      </section>
   </main> 
   </>
  );
