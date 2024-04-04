3c1469ea2c38a93f2bab04a4c775710b65f75e64
SarayAnta <s.rodriguezanta@gmail.com>
2024-04-04 15:28:56 +0200

    a mamarla

--- backend/database/connection_db.ts
+++ backend/database/connection_db.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
-import { DB_NAME, DB_USER, DB_PASSWORD } from '../config'
+import { DB_DEV_NAME, DB_USER, DB_PASSWORD } from '../config'
 
 
-const connection_db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+const connection_db = new Sequelize(DB_DEV_NAME, DB_USER, DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
 });
