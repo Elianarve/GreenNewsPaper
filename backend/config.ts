1b511ee1688c563d7c53fbacf8d0b4eab39531ca
SarayAnta <s.rodriguezanta@gmail.com>
2024-04-05 09:22:48 +0200

    create database for tests

--- backend/config.ts
+++ backend/config.ts
@@ -6,3 +6,5 @@ export const DB_USER: string = process.env.DB_USER || "";
 export const DB_PASSWORD: string = process.env.DB_PASSWORD || ""; 
 export const PORT = process.env.PORT || 8000;
 export const TK_SECRET = process.env.TK_SECRET || "";
+export const DB_TEST_NAME = process.env.DB_TEST_NAME || "";
+export const NODE_ENV = process.env.NODE_ENV;
