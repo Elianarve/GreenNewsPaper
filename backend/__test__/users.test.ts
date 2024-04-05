52be224d60e7bc7cbd41e897eb36f5b05d38af35
SarayAnta <s.rodriguezanta@gmail.com>
2024-04-05 14:18:42 +0200

    POST and GET test

--- backend/__test__/users.test.ts
+++ backend/__test__/users.test.ts
@@ -1,20 +1,38 @@
 import request from 'supertest';
 import { app, server } from '../app';
 import connection_db from '../database/connection_db';
+import UsersModel from '../models/userModel';
+import { User } from '../interfaces/userInterface';
 
 const api = request(app);
 
-test('Simple test', () => {
-    expect(2+2).toBe(4);
-});
+describe('TESTING CRUD users', ()=> {
 
-test('GET', async() => {
-    const response = await api.get('/users')
-    expect(response.status).toBe(200)
+    describe('GET', () => {
+        test('GET Response body must be an array and then show 200 status', async() => {
+            const response = await api.get('/users')
+            expect(Array.isArray(response.body)).toBe(true)
+            expect(response.status).toBe(200)
+        })
+    })
+
+    describe('POST', () => {
+        test('POST response should be an object and then return status 201', async() => {
+            const response = await api.post('/users').send({
+                "name": "test",
+                "email": "test@gmail.com",
+                "password": "Unacontraseña!1"
+            })
+            console.log('HOLA MIERDA')
+            expect(response.status).toBe(201)
+            expect(typeof response).toBe('object')
+        })
+    })
+    
+    
+    afterAll( async () => {
+        server.close();
+        await connection_db.sync({force: true });
+        console.log('All databases are clean')
+     });
 })
-
-afterAll( async () => {
-    server.close();
-    await connection_db.sync({force: true });
-    console.log('All databases are clean')
- })
