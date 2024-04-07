f4298290c1cce631f589f01c3cc8821d24ef1ada
jessica noguera <jnoguera.fact@gmail.com>
2024-04-08 00:42:43 +0200

    cambios

--- backend/validators/newsValidator.ts
+++ backend/validators/newsValidator.ts
@@ -1,27 +1,4 @@
 import { check } from "express-validator";
-import  validateResultNews  from '../helpers/validateResult';
-import { Request, Response, NextFunction } from 'express';
-
-const newsValidator =[
-    check('title')
-        .exists()
-        .notEmpty(),
-    check('date')
-        .exists()
-        .notEmpty()
-        .isDate(),
-    check('description')
-        .exists()
-        .notEmpty(), 
-    check('image')
-        .exists(),
-    (req: Request, res: Response, next: NextFunction) =>{
-        validateResultNews(req, res, next)
-    }
-]
-
-export default newsValidator;
-import { check } from "express-validator";
 import  validateResultNews  from '../helpers/validateResultNews';
 import { Request, Response, NextFunction } from 'express';
 
