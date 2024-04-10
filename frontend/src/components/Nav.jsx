8e9bff8fa0bc56cacbd2192df3e8fb50c000af9d
Elianarve <elirve2@outlook.es>
2024-04-10 10:33:02 +0200

    add

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -5,7 +5,8 @@ import profile from '../assets/profile-icon.svg'
 import save from '../assets/save-icon.svg'
 
 const Nav = () => {
-  const {state} = useLocation();
+  // const {state} = useLocation();
+  // console.log(state.name);
   
     return (
     <>
@@ -22,7 +23,7 @@ const Nav = () => {
            </Link>
            <li className="text-white bg-[#222222] px-4 py-2 rounded-lg hover:bg-[#FB005A] active:bg-[#B800B0] focus:outline-none focus:ring focus:ring-violet-300"><img className='mt-1' src={save}></img></li>
            <li className="text-white size-9"><img src={profile}></img></li>
-           <button className="text-white bg-[#222222] px-4 py-2 rounded-lg hover:bg-[#FB005A] active:bg-[#B800B0] focus:outline-none focus:ring focus:ring-violet-300">{state.name}</button>
+           <button className="text-white bg-[#222222] px-4 py-2 rounded-lg hover:bg-[#FB005A] active:bg-[#B800B0] focus:outline-none focus:ring focus:ring-violet-300">Name</button>   {/*{state.name} */}
          </ul>
         </nav>
     </>
