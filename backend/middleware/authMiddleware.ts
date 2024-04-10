9f9e24e5e974994ffbc6dee51c277277a9dfc04e
Elianarve <elirve2@outlook.es>
2024-04-10 10:34:43 +0200

    add-modified

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -6,8 +6,8 @@ export const authToken = (req: Request, res: Response, next: NextFunction) =>{
         const authToken = req.headers.authorization?.split(' ')[1];
         const dataToken: any = verifyToken(authToken);
         const userId: number = dataToken.userId;
-        console.log(dataToken)
-        console.log(userId)
+        // console.log(dataToken)
+        // console.log(userId)
         req.body.author_id = userId;
     next();
     } catch (error) {
