3cb25c799172025f1fcd0842f8992db1f7357cae
jessica noguera <jnoguera.fact@gmail.com>
2024-04-12 08:52:04 +0200

    cambios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import router from './router/router.jsx';
+import './main.css';
+import UserProvider from "./context/UserContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <UserProvider>
+      <Router>
+        {router}
+      </Router>
+    </UserProvider>
+  </React.StrictMode>
+);
