69af6aa4322f641fc18ab28ce0cdc9fb71bc3b78
RaquelLores <rlorescasalinas@gmail.com>
2024-04-12 11:01:51 +0200

    fix-backend-authRouter-conflict

--- backend/routes/authRouter.ts
+++ backend/routes/authRouter.ts
@@ -1,12 +1,8 @@
 import express from 'express';
 import { register, login } from '../controllers/authController';
-<<<<<<< HEAD
-
-=======
 import { userValidator } from '../validators/usersValidator';
 import handleValidationResults from '../helpers/validationHelper';
 // import { authToken } from '../middleware/authMiddleware';
->>>>>>> feature-validations
 
 const router = express.Router();
 
