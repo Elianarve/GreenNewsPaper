68e83ac702d488dabf8449266102ff643cce40dc
SarayAnta <s.rodriguezanta@gmail.com>
2024-04-12 11:11:32 +0200

    delete password validation in loginForm

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -16,11 +16,6 @@ const LoginForm = () => {
 
   const validationSchema = Yup.object().shape({
     email: Yup.string().email('El email debe ser válido.').required('El email es requerido.'),
-    password: Yup.string().required('La constraseña es requerida').min(8, 'La contraseña debe tener al menos 8 caracteres')
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/,
-      'La contraseña debe contener al menos una minúscula, una mayúscula, un número y un caracter especial (!@#$%^&*(),.?":{}|<>) y debe tener al menos 8 caracteres.'
-    ),
 });
 
   const handleSubmit = async (e) => {
