29b3d0e129b568807d8a9edfa745d1315df1b014
Elianarve <elirve2@outlook.es>
2024-04-16 20:14:35 +0200

    update-modals

--- backend/validators/newsValidator.ts
+++ backend/validators/newsValidator.ts
@@ -4,24 +4,12 @@ const newsValidator =[
     check('title')
         .notEmpty().withMessage('The title field must not be empty.')
         .isString().withMessage('The description field must be a string')
-        .isLength({min: 3, max: 25}).withMessage('The title field must be between 3 and 25 characters long.'),
-
-    // check('date')
-    //     .notEmpty().withMessage('The date field must not be empty.')
-    //     .isDate().withMessage('The date field must be a date.')
-    //     .isISO8601().withMessage('The date field must be in the format YYYY-MM-DD.')
-    //     .custom((value) => {
-    //         const newDate = new Date(value);
-    //         const currentDate = new Date();
-    //         if ( newDate > currentDate ) {
-    //             throw new Error('The date field must be equal to or earlier than the current date.')
-    //         }
-    //         return true;
-    //     }),
+        .isLength({min: 3, max: 40}).withMessage('The title field must be between 3 and 40 characters long.'),
 
     check('description')
         .notEmpty().withMessage('The description field must not be empty.')
-        .isString().withMessage('The description field must be a string'),
+        .isString().withMessage('The description field must be a string')
+        .isLength({min: 25, max: 60000}).withMessage('The title field must be between 25 and 60000 characters long.'),
 
     check('image')
         .notEmpty().withMessage('The image field must not be empty.')
