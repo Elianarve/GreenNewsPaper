27d3149391421b92a23db5eee14a49881bd547b3
BeatrizMercado <bea.mercado.lop@gmail.com>
2024-04-16 18:06:30 +0200

    add-style-with-css-to-landing-desktop

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useUserContext } from '../context/UserContext.jsx';
 import { loginUser } from '../services/logReg.js';
-import * as Yup from 'yup';
+// import * as Yup from 'yup';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
@@ -36,23 +36,21 @@ const LoginForm = () => {
         setPasswordError('Error en la solicitud de inicio de sesión');
         setEmailError('');
       }
-      // Aquí podemos manejar errores, ejem. mostrar un mensaje al usuario
-     }
+    }
 
-     //onResetForm();
   };
 
 
  return (
     <>
-      <form onSubmit={handleSubmit} className="px-8 pt-6 pb-8 mb-4">
+      <form onSubmit={handleSubmit} className="px-8 pb-8 mb-4">
         <div className="mb-4">
           <label className="block text-white font-poppins mb-2 text-left" htmlFor="email">
             Email
             <input type="email" value={email} onChange={(e) => {
               setEmail(e.target.value);
-              setEmailError('');}} required className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" placeholder="hola.soy.bea@gmail.com"/>
-              {emailError && <p className="text-[#FB005A] text-xs">{emailError}</p>}
+              setEmailError('');}} required className="font-poppins shadow appearance-none bg-[#222222] rounded-lg text-slate-50 w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline h-12" id="email" placeholder="hola.soy.bea@gmail.com"/>
+              {emailError && <p className="text-[#FB005A] text-xs mt-2">{emailError}</p>}
           </label>
         </div>
 
@@ -61,15 +59,15 @@ const LoginForm = () => {
             Contraseña
             <input type="password" value={password} onChange={(e) =>{
                setPassword(e.target.value);
-               setPasswordError('');}} required className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" placeholder="Ingresa tu contraseña"/>
+               setPasswordError('');}} required  className="font-poppins shadow appearance-none bg-[#222222] rounded-lg text-slate-50 w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline h-12" id="password" placeholder="Ingresa tu contraseña"/>
                {passwordError && <p className="text-[#FB005A] text-xs">{passwordError}</p>}
           </label>
         </div>
         <div className="flex flex-col items-center">
-          <button className="w-full font-poppins bg-gradient-to-r rounded-lg from-[#B800B0] to-[#FB005A] hover:from- [#FB005A] hover:to-[#B800B0] text-white py-2 px-4 focus:outline-none focus:shadow-outline mb-5 h-12" type="submit">
+            <button className="w-full font-poppins bg-gradient-to-r rounded-lg from-[#B800B0] to-[#FB005A] hover:from- [#FB005A] hover:to-[#B800B0] text-white py-2 px-4 focus:outline-none focus:shadow-outline mb-5 h-12" type="submit">
             Iniciar sesión
-          </button>
-          <p className="font-poppins text-[#9E9E9E] justify-center">¿No tienes cuenta? <Link to="/register" className="text-white">Regístrate</Link></p>
+            </button>
+            <p className="font-poppins text-[#9E9E9E] justify-center">¿No tienes cuenta? <Link to="/register" className="text-white">Regístrate</Link></p>
         </div>
       </form>
     </>
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Spline from '../components/Spline.jsx'
-// import Nav from '../components/Nav.jsx'
+import './css/Landing.css'
 
 const Landing = () => {
  return (
   <>
-  {/* <Nav/> */}
-  <main className="grid grid-cols-2 place-content-center bg-neutral-900">
+  {/* <main className="grid grid-cols-2 place-content-center bg-neutral-900">
       <section className="items-center mx-32 mt-20 justify-center min-h-screen bg-gray-00">
-          <h1 className="font-poppins font-semibold text-6xl mb-4 text-white">¡Explora el <br/>mañana <br/><span className="text-[#FB005A]">hoy mismo!</span></h1>
+          <h1 className="font-poppins font-semibold mb-4 text-6xl text-white">¡Explora el <br/>mañana <br/><span className="text-[#FB005A]">hoy mismo!</span></h1>
           <p className='font-poppins mb-5 text-lg text-white'>Alimenta tu curiosidad diaria: <br/>tecnología, IA, blockchain y más.</p>
           <Link to="/register">
-            <button className="font-poppins bg-gradient-to-r rounded-lg from-[#B800B0] to-[#FB005A] hover:from- [#FB005A] hover:to-[#B800B0] text-white py-2 px-4 focus:outline-none focus:shadow-outline mb-5 w-11/12 h-12">
+            <button className="font-poppins bg-gradient-to-r rounded-lg from-[#B800B0] to-[#FB005A] hover:from- [#FB005A] hover:to-[#FB005A] text-white py-2 px-4 focus:outline-none focus:shadow-outline mb-5 w-11/12 h-12">
               Regístrate
             </button>
           </Link>
           <Link to ="/login">
-            <button className="font-poppins font-semibold bg-white rounded-lg hover:from-[#FB005A]   hover:to-purple-600 text-black py-2 px-4 focus:outline-none focus:shadow-outline w-11/12 h-12">
+            <button className="font-poppins font-semibold bg-white rounded-lg hover:from-[#FB005A] hover:to-[#FB005A] text-black py-2 px-4 focus:outline-none focus:shadow-outline w-11/12 h-12">
               Iniciar sesión
             </button>
           </Link>
       </section>
       <section className='mr-20 mb-20'>
+          <Spline/>
+      </section>
+  </main>  */}
+
+  <main className="landing">
+      <section className="section-left size-fit">
+          <h1 className='landing-title'>¡Explora el <br/>mañana <br/><span className="text-[#FB005A]">hoy mismo!</span></h1>
+          <p className='landing-text'>Alimenta tu curiosidad diaria: <br/>tecnología, IA, blockchain y más.</p>
+          {/* <div  className='buttons'> */}
+            <Link to="/register">
+              <button className='register-btn'>
+                Regístrate
+              </button>
+            </Link>
+            <Link to ="/login">
+              <button className='login-btn'>
+                Iniciar sesión
+              </button>
+            </Link>
+          {/* </div> */}
+      </section>
+      <section className='section-right'>
           <Spline/>
       </section>
   </main> 
